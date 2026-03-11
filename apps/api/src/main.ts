d1a27a4ea637755c91bb907eeab82f746c1dd57b
Brooks <brooks.forsyth@gmail.com>
2026-03-10 23:22:42 -0400

    made host 0.0.0.0

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,8 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const port = process.env["PORT"] ?? 3001;
-  await app.listen(port);
-  console.log(`API running on http://localhost:${port}`);
+  const host = process.env["HOST"] ?? "0.0.0.0";
+  await app.listen(port, host);
+  console.log(`API running on ${await app.getUrl()}`);
 }
 
 void bootstrap();
